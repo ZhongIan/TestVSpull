3c0dd993701c395e669145ef45797628c83abaac
ZhongIan <po39086m@yahoo.com.tw>
2020-12-02 14:10:02 +0800

    init

---
+++ WebApplication1/ViewModels/Prod/v菜品VM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels.Prod
+{
+    public class v菜品VM
+    {
+        [Key]
+        public int fVID { get; set; }
+
+        public string 項目名稱 { get; set; }
+
+        [Display(Name = "產品編號")]
+        public int fPID { get; set; }
+
+        [Display(Name = "菜品簡介")]
+        [Required(ErrorMessage = "您必須輸入菜品簡介！")]
+        public string f菜品簡介 { get; set; }
+
+        [Display(Name = "菜品照片")]
+        public string f菜品照片 { get; set; }
+
+        [Display(Name = "菜品名稱")]
+        [Required(ErrorMessage = "您必須輸入菜品名稱！")]
+        public string f菜品名稱 { get; set; }
+
+        public HttpPostedFileBase image { get; set; }
+
+    }
+}
